737533f0ec813d6575c0a7fdea6ff482d2576599
hoangnym <hoangnym94@gmail.com>
2022-06-15 22:23:56 +0200

    thumbnail done

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div className="">
       <Head>
@@ -16,10 +18,26 @@ export default function Home() {
       </Head>
 
       <Header />
-
       <Nav />
-
-      {/* Results */}
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  console.log(genre);
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
